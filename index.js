08e82d695d3fb0d683b8c6b6156c892194c3244a
Jelyn Corners <99438112+jelyncorners@users.noreply.github.com>
2022-11-28 16:46:53 -0800

    fixing port on index.js

--- index.js
+++ index.js
@@ -72,5 +72,9 @@ app.get('/account/alldata', function (req, res)  {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`running on port: ${PORT}`));
+//const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => console.log(`running on port: ${PORT}`));
+
+var port = 3000;
+app.listen(process.env.PORT || port);
+console.log('Running on port: ' + port);
