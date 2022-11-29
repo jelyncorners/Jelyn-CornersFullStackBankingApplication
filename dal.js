5dd374a2df81bd8b86697e4285863a4157c79c2a
Jelyn Corners <99438112+jelyncorners@users.noreply.github.com>
2022-11-28 16:45:06 -0800

    fixing dal

--- dal.js
+++ dal.js
@@ -1,77 +1,85 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://127.0.0.1:27017';
-//const url         = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017';
-let db            = null;
- 
-// connect to mongo
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-    console.log("Connected successfully to Jelyn server");
+const uri = process.env.MONGODB_URI;
+let db = null;
 
+// connect to mongo
+MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, client) {
+    console.log("Connected successfully to db server");
+    console.log(uri);
     // connect to myproject database
     db = client.db('myproject');
 });
 
-//create user account
+// create user account using the collection.insertOne function
 function create(name, email, password) {
+    // TODO: populate this function based off the video
     return new Promise((resolve, reject) => {
         const collection = db.collection('users');
         const doc = {name, email, password, balance: 0};
         collection.insertOne(doc, {w:1}, function(err, result) {
-            err ? reject(err) : resolve(doc);
+            err ? reject(err)  :  resolve(doc);
         });
     });
 }
 
 // find user account
-function find(email){
-    return new Promise((resolve, reject) => {    
-        const customers = db
-            .collection('users')
-            .find({ email: email })
-            .toArray(function(err, docs) {
-                err ? reject(err) : resolve(docs);
-        });   
-    });
-}
-
-
-//find one user acccount
 function findOne(email) {
     return new Promise((resolve, reject) => {
         const customers = db
-        .collection('users')
-        .findOne({email: email})
-        .then((doc) => resolve(doc))
-        .catch((err) => reject(err))
-    });
+            .collection('users')
+            .findOne({ email: email })
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));
+    })
 }
 
-//update after deposit or withdrawal
-function update(email, amount) {
+// update withdraw amount
+function withdrawUpdate(email, amount) {
+    console.log('calling the balance change' + amount);
     return new Promise((resolve, reject) => {
         const customers = db
-        .collection('users')
-        .findOneAndUpdate(
-            { email: email },
-            { $inc: { balance: amount }}, 
-            { returnOriginal: false },
-            function(err, documents) {
-                err ? reject(err) : resolve(documents);
-            }
-        );
+            .collection('users')
+            .findOneAndUpdate(
+                { email: email },
+                { $inc: { balance: -amount } },
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );
     });
 }
 
-//all users
+// update withdraw amount
+function depositUpdate(email, amount) {
+    console.log('calling the balance change' + amount);
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .findOneAndUpdate(
+                { email: email },
+                { $inc: { balance: amount } },
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );
+    });
+}
+
+
+// return all users by using the collection.find method
 function all() {
+    // TODO: populate this function based off the video
     return new Promise((resolve, reject) => {
         const customers = db
             .collection('users')
             .find({})
             .toArray(function(err, docs) {
-                err ? reject(err) : resolve(docs);
+                err ? reject(err)  :  resolve(docs);
             });
-    });
+    })
 }
 
-module.exports = { create, find, findOne, update, all };
+
+module.exports = { create, findOne, depositUpdate, withdrawUpdate, all};
